ab2197a2e672bc6a428d54e1ed37bbe199fe3433
StephenWray <40370064+StephenWray@users.noreply.github.com>
2019-09-26 21:59:30 -0500

    initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Titles extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title-container__title">Weather Forecast</h1>
+                <h3 className="title-container__subtitle">Keeping you up to date with your local forecasts</h3>
+            </div>
+        )
+    }
+}
+
+export default Titles
